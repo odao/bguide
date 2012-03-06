7b6658c34b526793a8823080efb14d7cebd438cd
Michael Chow <zmjq08@gmail.com>
2012-03-06 11:27:38 +0800

    server push clock

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ var app = require('express').createServer(),
 app.listen(9999);
 
 app.get('/', function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    //res.writeHead(200, {'Content-Type': 'text/plain'});
     res.sendfile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function(socket){
-    socket.emit('', {});    
+    var pushTime = function(){
+        socket.emit('nowtime', {now: new Date()});    
+   };
+    setInterval(pushTime, 5*1000);
 });
     
